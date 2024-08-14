6fd8536049b5f7cceda5588db7c4b2a34428f99d
epen3420 <reijiwake@outlook.com>
2024-08-14 21:36:57 +0900

    Revert "8/13"
    This reverts commit 7dd106c7d9110f4dc5589bd3be9ca076793a4d86.

--- Unity_shogi/Assets/Scripts/GameManager.cs
+++ Unity_shogi/Assets/Scripts/GameManager.cs
@@ -6,5 +6,62 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+  private bool RoundWinner;
+  private int Next_Koma;
+  private MainSceneManager mainSceneManager;
 
+
+  void Start()
+  {
+    mainSceneManager = GameObject.Find("MainSceneManager").GetComponent<MainSceneManager>();
+    mainSceneManager.Now_ally = Next_Koma;
+  }
+
+  void Update()
+  {
+    if (mainSceneManager.RoundEnd)
+    {
+      RoundWinner = mainSceneManager.Player_which;
+      Debug.Log(RoundWinner);
+      StartCoroutine(Gameloop());
+    }
+  }
+
+  private IEnumerator Gameloop()
+  {
+    /* mainSceneManager.RoundProcess = true;
+    yield return new WaitForSeconds(0.5f);
+    IsWinner();
+    yield return new WaitForSeconds(0.5f); */
+    yield return new WaitForSeconds(0.5f);
+    Next_Koma++;
+    Reset();
+  }
+
+  /*   private void IsWinner()
+    {
+      RoundWinner = !mainSceneManager.Player_which;
+      if (RoundWinner)
+      {
+        mainSceneManager.Now_ally++;
+      }
+      else
+      {
+        mainSceneManager.Now_Enemy++;
+      }
+    } */
+
+  private void Reset()
+  {
+    SceneManager.sceneLoaded += GameSceneLoaded;
+
+    SceneManager.LoadScene("MainScene");
+  }
+
+  private void GameSceneLoaded(Scene next, LoadSceneMode mode)
+  {
+    var gameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
+    gameManager.Next_Koma = Next_Koma;
+    SceneManager.sceneLoaded -= GameSceneLoaded;
+  }
 }
--- Unity_shogi/Assets/Scripts/KomaController.cs
+++ Unity_shogi/Assets/Scripts/KomaController.cs
@@ -7,37 +7,23 @@ public class KomaController : MonoBehaviour
 {
     //スクリプトInputManagerを参照するための命名
     private InputManager inputManager;
-    //スワイプの強さ
-    private float SwipeForceMultiplier = 10f;
-    //ゼロ判定用
-    private Vector3 ZeroVec = new Vector3(0.0f, 0.0f, 0.0f);
-    private bool _CanInput;
     //スワイプ状況
     public bool CanSwipe;
+    //スワイプの強さ
+    private float SwipeForceMultiplier = 10f;
+
 
     void Start()
     {
-        //スワイプ状況の初期化
         CanSwipe = true;
         //InputManagerの参照
         inputManager = GameObject.Find("InputManager").GetComponent<InputManager>();
     }
 
-    void Update()
-    {
-        _CanInput = inputManager.CanInput;
-    }
     public void SwipeKoma(Rigidbody rb)
     {
-        Debug.Log("x");
-        //ユーザー入力が終わっていなかったら先に行かない
-        if (_CanInput) return;
-        Debug.Log("ys");
         //InputManagerのスワイプの距離を参照
-        Vector3 SwipeDistance = inputManager.GetSwipe();
-        //スワイプ距離が0だったら先に行かない
-        if (SwipeDistance == ZeroVec) return;
-        Debug.Log("z");
+        Vector3 SwipeDistance = inputManager.SwipeMouse();
         //スワイプの方向の算出
         Vector3 SwipeDirection = -SwipeDistance.normalized;
         //スワイプの力の算出
