a491a90449d211700bd225217805cc1ee24ea354
epen3420 <reijiwake@outlook.com>
2024-08-16 17:13:35 +0900

    8/16
    make change title. add blank before swiping mouse.

--- Unity_shogi/Assets/Scripts/UIManager.cs
+++ Unity_shogi/Assets/Scripts/UIManager.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] Text TextTurnInfo;
+    [SerializeField] public Text TextTurnInfo;
+    [SerializeField] public Text TextWinnerInfo;
+    [SerializeField] public Button ButtonTitle;
 
 
     public void TurnInfo(bool isTurn)
@@ -17,4 +21,23 @@ public class UIManager : MonoBehaviour
         }
         else { TextTurnInfo.text = "2Pの番です"; }
     }
+    public void WinnerInfo(bool isWinner)
+    {
+        TextWinnerInfo.gameObject.SetActive(true);
+        if (isWinner)
+        {
+            TextWinnerInfo.text = "1Pの勝利";
+        }
+        else
+        {
+
+            TextWinnerInfo.text = "2Pの勝利";
+        }
+    }
+
+    public void SwitchScene()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+
 }
