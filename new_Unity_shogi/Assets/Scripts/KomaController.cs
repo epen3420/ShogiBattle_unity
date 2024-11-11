0801bfd1b7d9fce32ad3c74bf022ab8c682afeda
epen3420 <reijiwake@outlook.com>
2024-11-11 20:22:21 +0900

    進行中

---
+++ new_Unity_shogi/Assets/Scripts/KomaController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class KomaController : MonoBehaviour, IKomaAction
+{
+    private Rigidbody rb;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 moveVector)
+    {
+        rb.AddForce(moveVector, ForceMode.Impulse);
+
+        Debug.Log($"Koma move vector: {moveVector}");
+    }
+}
